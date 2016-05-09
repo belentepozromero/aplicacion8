9cd1659607df4b1f67716dd56729f14c87adaa63
belentepozromero <btepozromero@gmail.com>
2016-05-09 13:54:02 -0500

    8subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,25 +1,27 @@
 // JavaScript Document
 
-$(document).ready(function(e){
-	document.addEventListener ("deviceready",function(){
+$(document).ready(function (e){
+	document.addEventListener("deviceready",function(){
 		
-		$('#izquierda').on(" swipeleft",function(){
-			navigator.notification.alert ("Dezlizo a la izquierda", function(){"Aplicacion5"," Aceptar"});
+		$('#izquierda').on("swipeleft",function(){
+			navigator .notification. alert("Deslizo a la izquierda",function(){"Aplicacion5","Aceptar"});
 		});
 		
-		$('#derecha').on(" swiperight",function(){
-			navigator.notification.confirm ("¿Que quieres hacer?", function(opt){
-	switch(opt)
-	{
-		case 1:
-		navigator.notification.beep(1);
-		break;
-		case 2:
-			navigator.notification.vibrate(1000);
-			break;
-		}
-		
-	},"Aplicacion8","Beep,Vibrar,Cancelar");
-});
-	},false);
-});
+		$('#derecha').on("swiperight",function(){
+			navigator .notification. confirm ("¿Que quieres hacer?",function(opt){
+				switch(opt)
+				{
+					case 1:
+					navigator.notification.beep(1);
+					break;
+					
+					case 2:
+					navigator.notification.vibrate(1000);
+					break;
+				}
+	   },"Aplicacion8","Beep,Vibrar,Cancelar");
+			
+   });
+	    },false);
+   });
+			
