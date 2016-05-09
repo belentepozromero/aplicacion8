007c740b9c61a54a0a511ed4853cc76fc801ebd9
belentepozromero <btepozromero@gmail.com>
2016-05-09 13:00:22 -0500

    4subida

--- js/acciones.js
+++ js/acciones.js
@@ -7,7 +7,7 @@ $(document).ready(function(e){
 			navigator.notification.alert ("Dezlizo a la izquierda", function(){"Aplicacion5"," Aceptar"});
 		});
 		
-		$('#derecha').on(" swipeleft",function(){
+		$('#derecha').on(" swiperight",function(){
 			navigator.notification.confirm ("¿Que quieres hacer?", function(opt){
 	switch(opt)
 	{
